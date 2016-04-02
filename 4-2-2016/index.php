44f3100c9401a6d003dc9132c1bab39cd2557426
Bryan Vale <bvalarez@stevens.edu>
2016-04-02 10:59:55 -0400

    Header added, nav and search base

--- 4-2-2016/index.php
+++ 4-2-2016/index.php
@@ -6,6 +6,23 @@
 
 </head>
 <body>
-	<img src="bw_logo.png" alt="Stevens Institute of Technology - The Innovation University">
+    <hedaer>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Help</a></li>
+            </ul>
+        </nav>
+        <div id="search">
+            <form>
+                <input type="text" placeholder="Search..." required>
+                <input type="button" value="Search">
+            </form>
+        </div>
+    </hedaer>
+    <div id="wrapper">
+	   <img src="bw_logo.png" alt="Stevens Institute of Technology - The Innovation University">
+    </div>
 </body>
 </html>
